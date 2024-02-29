15fe6b74d3db80983d903fad91347367660d22fc
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-02-28 23:24:00 -0500

    adjusted tailwind

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { cva } from "class-variance-authority";
+
+const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+    
+})
+
+export function Button () {
+    return <button />
+}
