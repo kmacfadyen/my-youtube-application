9a7bf365b4f47a59dd78170a9c89b0772a4f8fa8
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-03-10 17:02:30 -0400

    Categories highlight correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { PageHeader } from "./layouts/PageHeader";
 import { CategoryPills } from "./components/CategoryPills";
 import { categories } from "./data/home";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return <div className="max-h-screen flex flex-col">
     <PageHeader />
     <div className="grid grid-cols-[auto,1fr] 
     flex-grow-1 overflow-auto"></div>
     <div>Sidebar</div>
     <div className="sticky top-0 bg-white z-10 pb-4">
-      <CategoryPills categories={categories}/>
+      <CategoryPills 
+      categories={categories}
+      selectedCategory={selectedCategory}
+      onSelect={setSelectedCategory}
+      />
     </div>
   </div>
 }
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,17 +1,20 @@
 import { Button } from "./Button";
 
 type CategoryPillProps = {
-    categories: string[]
+    categories: string[],
+    selectedCategory: string,
+    onSelect: (category: string) => void
 }
 
-export function CategoryPills ({ categories }: CategoryPillProps) {
+export function CategoryPills ({ categories, selectedCategory, onSelect }: CategoryPillProps) {
     return <div className="overflow-x-hidden relative">
         <div className="flex whitespace-nowrap gap-3 transition-transform
         w-[max-content]">
             {categories.map(category => (
                 <Button 
                     key={category}
-                    variant="dark"
+                    onClick={() => onSelect(category)}
+                    variant={selectedCategory === category ? "dark" : "default"}
                     className="py-1 px-3 rounded-lg whitespace-nowrap"
                 >
                     {category}
