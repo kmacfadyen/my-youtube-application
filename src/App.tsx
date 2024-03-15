d8c2ffa64d0106d92704611577ff491bbf62c055
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-03-14 23:48:36 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ function App() {
     <div className="grid grid-cols-[auto,1fr] 
     flex-grow-1 overflow-auto"></div>
     <div>Sidebar</div>
-    <div className="sticky top-0 bg-white z-10 pb-4">
-      <CategoryPills 
-      categories={categories}
-      selectedCategory={selectedCategory}
-      onSelect={setSelectedCategory}
-      />
+    <div className="overflow-x-hidden px-8 pb-4">
+      <div className="sticky top-0 bg-white z-10 pb-4">
+        <CategoryPills 
+          categories={categories}
+          selectedCategory={selectedCategory}
+          onSelect={setSelectedCategory}
+        />
+      </div>
     </div>
   </div>
 }
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Button } from "./Button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type CategoryPillProps = {
     categories: string[],
@@ -6,21 +8,53 @@ type CategoryPillProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 export function CategoryPills ({ categories, selectedCategory, onSelect }: CategoryPillProps) {
-    return <div className="overflow-x-hidden relative">
-        <div className="flex whitespace-nowrap gap-3 transition-transform
-        w-[max-content]">
-            {categories.map(category => (
+    const [isLeftVisible, setIsLeftVisible] = useState(false);
+    const [isRightVisible, setIsRightVisible] = useState(true);
+
+    return (
+        <div className="overflow-x-hidden relative">
+            <div className="flex whitespace-nowrap gap-3 transition-transform
+            w-[max-content]">
+                {categories.map(category => (
+                    <Button 
+                        key={category}
+                        onClick={() => onSelect(category)}
+                        variant={selectedCategory === category ? "dark" : "default"}
+                        className="py-1 px-3 rounded-lg whitespace-nowrap"
+                    >
+                        {category}
+                    </Button>
+                ))}
+            </div>
+            {isLeftVisible && (
+            <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r
+            from-white from-50% to-transparent w-24 h-full"
+            >
                 <Button 
-                    key={category}
-                    onClick={() => onSelect(category)}
-                    variant={selectedCategory === category ? "dark" : "default"}
-                    className="py-1 px-3 rounded-lg whitespace-nowrap"
+                    variant="ghost" 
+                    size="icon" 
+                    className="h-full aspect-square w-auto p-1.5"
                 >
-                    {category}
+                    <ChevronLeft />
                 </Button>
-            ))}
-            
+            </div>
+            )}
+            {isRightVisible && (
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l
+            from-white from-50% to-transparent w-24 h-full flex justify-end"
+            >
+                <Button 
+                    variant="ghost" 
+                    size="icon" 
+                    className="h-full aspect-square w-auto p-1.5"
+                >
+                    <ChevronRight />
+                </Button>
+            </div>
+            )}
         </div>
-    </div>
+    )
 }
