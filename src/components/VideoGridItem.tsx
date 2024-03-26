0f7b5a6ce746e2559cb110dc3d93ce82e3e5d687
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-03-25 23:56:42 -0400

    working on grid

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -13,6 +13,15 @@ type VideoGridItemProps = {
     videoUrl: string
 }
 
-export function VideoGridItem () {
-
+export function VideoGridItem ({
+    id, title, channel, views, postedAt, duration, thumbnailUrl, videoUrl,
+}: VideoGridItemProps) {
+    return <div className="flex flex-col gap-2">
+        <a href={`/watch?v=${id}`} className="relative aspect-video">
+            <img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />
+            <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-.5 rounded">
+                {duration}
+            </div>
+        </a>
+    </div>
 }
